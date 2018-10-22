9b334ceac4d0de070427740142262fccad48bae4
IrfaanKhalid <irfaan.khalid@vanderbilt.edu>
2018-10-22 18:11:00 -0500

    updated deps

--- cs4278-assignment5/project.clj
+++ cs4278-assignment5/project.clj
@@ -4,7 +4,7 @@
   :license {:name "Eclipse Public License"
             :url "http://www.eclipse.org/legal/epl-v10.html"}
     :dependencies [[org.clojure/clojure "1.7.0"]
-                 [twilio-api "1.0.1"]]
+                  [twilio-api "1.0.1"]]
   :main ^:skip-aot cs4278-assignment5.core
   :target-path "target/%s"
   :profiles {:uberjar {:aot :all}})
