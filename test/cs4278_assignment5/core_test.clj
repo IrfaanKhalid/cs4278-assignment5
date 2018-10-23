88f53171420241d0719810ed67913c763b67ea3a
IrfaanKhalid <irfaan.khalid@vanderbilt.edu>
2018-10-22 19:35:27 -0500

    adding tests

--- test/cs4278_assignment5/core_test.clj
+++ test/cs4278_assignment5/core_test.clj
@@ -2,6 +2,19 @@
   (:require [clojure.test :refer :all]
             [cs4278-assignment5.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+;; Unit tests
+;; Many functions cannot be easily tested deterministically, so we don't perform unit tests for them.
+;;
+
+(deftest get-contact-info-test
+  (testing "Testing get-contact-info"
+    (is (= (get-contact-info "white\") 
+            \"{:name \"Jules White\", :email \"jules.white@gmail.com\"}")))))
+
+(deftest get-contact-info-test
+  (testing "Testing get-final-data"
+    (is (= (get-final-data ["{:name " "Jules White" ", :email " "jules.white@gmail.com" "}"]) 
+            "Jules White can be reached at jules.white@gmail.com"))))
+
+;; Integration testing
+;; This is done by simply testing the code and seeing if a text message is received.
